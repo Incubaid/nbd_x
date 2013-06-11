9cf34b9f335081ec6a40c1dded82c5c94eb4b3fa
Romain Slootmaekers <romain@incubaid.com>
2013-06-11 10:56:41 +0200

    autoflush at 512 iso 1024 (because nbd-verify -a iops thinks we never ack otherwise)

--- cache.ml
+++ cache.ml
@@ -87,7 +87,7 @@ module CacheBlock (B: BLOCK) = (struct
     
     (* nbd-client sends no flushes *)
     let size = LbaMap.cardinal t.outstanding_writes in
-    let max_out = 1024 in (* arbitrary *)
+    let max_out = 512 in (* arbitrary, but 1024 seems too high for nbd-verify *)
     if size >  max_out
     then
       begin
